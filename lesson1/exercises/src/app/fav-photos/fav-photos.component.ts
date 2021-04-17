108ed819fcb7c4a449e1b18dbeddee1ec9c434eb
OJTang <oliverwunderlich13@gmail.com>
2021-04-17 00:58:22 -0500

    exercises complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.imgur.com/2DMpkJ3.gif';
+  image3 = 'https://www.nme.com/wp-content/uploads/2019/04/SPINAL_TAP_2_2000-1392x884.jpg';
 
   constructor() { }
 
